679b5b5c557cc2d956962a59e657c65c82e4c99e
Madhuwantha G K O <110708704+IT21802058@users.noreply.github.com>
2022-08-08 16:44:44 +0000

    convert c program to c++ program

--- tute01.cpp
+++ tute01.cpp
@@ -18,14 +18,13 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int main() 
 {
     float cm, inches;
-    cout<<"Enter a length in cm : ";
-    cin>>cm;
+    printf("Enter a length in cm : ");
+    scanf("%f",&cm);
     inches = cm / 2.54;
-    cout<<"Length in inches is <<inches;
+    printf("Length in inches is %f \n", inches);
     return 0;
 }  
